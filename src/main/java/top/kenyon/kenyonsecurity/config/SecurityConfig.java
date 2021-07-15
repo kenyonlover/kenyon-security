1898916b850475d490c2e8cb479bed9e301798b0
wuchengy <307980463@qq.com>
2021-07-15 23:22:59 +0800

    将用户数据存入数据库

--- src/main/java/top/kenyon/kenyonsecurity/config/SecurityConfig.java
+++ src/main/java/top/kenyon/kenyonsecurity/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package top.kenyon.kenyonsecurity.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
@@ -13,8 +14,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -25,6 +29,7 @@ import top.kenyon.kenyonsecurity.utils.RespBean;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -36,12 +41,52 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
+    /**
+     * 第一种
+     * 在内存中存入可以使用的用户以及用户可以使用的权限
+     * 此处配置会覆盖掉application.properties中配置的spring.security.user
+     * @param auth
+     * @throws Exception
+     */
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("kenyon").password("qwe123").roles("admin")
+//                .and()
+//                .withUser("tom").password("qwe123").roles("user");
+//    }
+
+    /**
+     * 第二种
+     * 在内存中存入可以使用的用户以及用户可以使用的权限
+     * @return
+     */
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withUsername("kenyon").password("qwe123").roles("admin").build());
+//        manager.createUser(User.withUsername("tom").password("qwe123").roles("user").build());
+//        return manager;
+//    }
+
+    @Autowired
+    DataSource dataSource;
+
+    /**
+     * 在数据库中存入可以使用的用户以及用户可以使用的权限
+     * 这个数据库的建表语句来源于Spring Security自带的SQL，文件名称为users.ddl
+     * @return
+     */
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("kenyon").password("qwe123").roles("admin")
-                .and()
-                .withUser("tom").password("qwe123").roles("user");
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+        if(!manager.userExists("kenyon"))
+            manager.createUser(User.withUsername("kenyon").password("qwe123").roles("admin").build());
+        if(!manager.userExists("tom"))
+            manager.createUser(User.withUsername("tom").password("qwe123").roles("user").build());
+        return manager;
     }
 
     @Override
